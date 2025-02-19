87fdad3910ecb20b0597ffbb877339d67fa17490
nandju <nlbmd17@gmail.com>
2025-02-19 14:42:51 +0100

    mot de ambassade

--- components/ambassade/ambassadeur/ambassadeur.tsx
+++ components/ambassade/ambassadeur/ambassadeur.tsx
@@ -7,7 +7,7 @@ export default function Ambassadeur() {
         L&apos;AMBASSADEUR
       </div>
       <div className="flex flex-col-reverse md:flex-row items-stretch justify-around gap-4 md:gap-12">
-        <div className="relative w-full max-w-96 h-[400px] md:h-[500px]">
+        <div className="relative w-full max-w-96 h-[400px] md:h-[650px]">
           <Image
             src="/assets/images/illustrations/ambassade/ambassadeur.png"
             alt="histoire-3"
@@ -26,35 +26,23 @@ export default function Ambassadeur() {
 
         <div className="flex flex-col gap-4 justify-between flex-1">
           <div className="text-secondary text-xl md:text-3xl font-semibold">
-            Bienvenue sur le Site de l&apos;Ambassade du Tchad en Côte d’Ivoire
+          Bienvenue sur le site de l&apos;Ambassade.
           </div>
           <div className="font-mulish text-sm md:text-base text-justify">
-            L&apos;Ambassade de la République du Tchad auprès de la République
-            de Turquie, placée sous l&apos;autorité de Son Excellence Monsieur
-            Gonfouli SOUARIBA, Ambassadeur Extraordinaire et Plénipotentiaire, a
-            été créée en 2014 avec Résidence à Ankara. Outre, son Ambassade dont
-            le siège se trouve à Ankara, capitale administrative et politique de
-            la Turquie, le Tchad dispose d&apos;un Consulat honoraire à
-            Istanbul. Il n&apos;est pas superflu de rappeler que
-            l&apos;Ambassade du Tchad en Turquie a pour vocation de promouvoir
-            de bonnes relations entre nos deux (02) pays (Tchad et Turquie).
-            L&apos;objectif principal de notre site est donc de fournir à tous
-            l&apos;actualité la plus récente dans le développement de nos
-            relations, les meilleures informations et services en vue de
-            faciliter les séjours en Turquie de nos compatriotes, et enfin,
-            d&apos;offrir aux visiteurs de tous horizons, une vision
-            authentique, multidimensionnelle et complète du Tchad. Nous avons
-            l&apos;immense plaisir de vous guider à travers le Tchad, Pays de
-            Toumaî, Berceau de l&apos;Humanité, avec ses paysages pittoresques
-            et variés, constitués des zones montagneuses, forêts, savanes,
-            déserts et surtout de deux (02) Sites inscrits sur la liste du
-            Patrimoine Mondial de l&apos;UNESCO à savoir les Lacs
-            d&apos;Ounianga et les Massifs de l&apos;Ennedi. Aussi, voudrais-je
-            profiter de cette opportunité pour vous souhaiter, au nom du
-            Gouvernement de la République du Tchad et au mien propre, la
-            cordiale bienvenue sur notre site.
+            La R&eacute;publique du Tchad, pays de Touma&iuml;, de par sa position g&eacute;ographique strat&eacute;gique, occupe une place de choix au c&oelig;ur du continent africain. Le pays, apr&egrave;s une transition politique r&eacute;ussie en trois ann&eacute;es, sous le leadership du Marechal du Tchad, Mahamat Idriss DEBY ITNO, Pr&eacute;sident de la R&eacute;publique, Chef de l&apos;&Eacute;tat, amorce des pas de g&eacute;ant pour son d&eacute;collage &eacute;conomique. Le programme politique du Chef de l&apos;&Eacute;tat est structur&eacute; autour de douze chantiers, comprenant chacun plusieurs actions notamment la paix, la s&eacute;curit&eacute; nationale, la promotion d&apos;un &Eacute;tat solidaire….
+            
+            Le programme de d&eacute;veloppement intitul&eacute; <span className="font-bold"> &quot;Vision 2030, le Tchad que nous voulons&quot; </span>, ax&eacute; sur l&apos;agenda 2063 de l&apos;Union Africaine et de l&apos;agenda 2030 des Nations Unies promulgu&eacute;s par les Chefs des &Eacute;tats membres de l&apos;Union Africaine est en cours d&apos;ex&eacute;cution. Ce programme bas&eacute; sur une ex&eacute;cution pragmatique a pour objectif de faire du Tchad un pays &eacute;mergent &agrave; l&apos;horizon 2030.
+            
+            L&apos;Ambassade du Tchad en R&eacute;publique de C&ocirc;te d&apos;Ivoire a juridiction dans quatre autres pays notamment : le Ghana, la Guin&eacute;e, la Sierra Leone et le Liberia. Les objectifs principaux de la mission sont le maintien et le renforcement des relations diplomatiques entre le Tchad et ces cinq pays. Cette diplomatie tous azimuts embrasse les domaines aussi vari&eacute;s que l&apos;&eacute;conomie, la culture et tout ce que comporte la coop&eacute;ration moderne entre les pays &eacute;pris de paix et ayant en partage des int&eacute;r&ecirc;ts communs.
+            
+            La mission de la Haute Repr&eacute;sentation du Tchad en C&ocirc;te d&apos;Ivoire consiste &eacute;galement &agrave; prot&eacute;ger les int&eacute;r&ecirc;ts de sa communaut&eacute; dans les cinq pays de sa juridiction, conform&eacute;ment aux usages diplomatiques.
+            
+            La plateforme qui vous est propos&eacute;e constitue une r&eacute;f&eacute;rence unique et efficace pour obtenir des informations g&eacute;n&eacute;rales sur le Tchad, afin de promouvoir efficacement le commerce, les investissements, le tourisme et la culture.
+            
+            Toute l&apos;&eacute;quipe de l&apos;Ambassade sera toujours &agrave; votre disposition pour vous r&eacute;server un accueil chaleureux, pas seulement aux Tchadiens, mais &agrave; tous ceux d&apos;autres pays qui souhaitent avoir des informations essentielles sur les services que nous offrons, notamment la d&eacute;livrance des visas et autres documents administratifs (carte consulaire, acte de naissance, acte de mariage et de d&eacute;c&egrave;s…).
+            
+            En attendant une visite physique que nous esp&eacute;rons proche, soyez les bienvenus au &quot;Tchad virtuel&quot;.
           </div>
-
           <div className="font-mulish font-extrabold md:font-bold text-lg text-center py-8">
             Sincères et chalereuses salutations <br /> Son Excellence Monsieur Gonfouli
             SOUARIBA, <br /> Ambassadeur de la République du Tchad auprès de la
